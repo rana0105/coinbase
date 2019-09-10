6dd617374804357f66838c5bb38d81111cf543a0
Rana <samrat0alam@gmail.com>
2019-09-11 00:31:49 +0600

    Coin Base

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class AdminController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //  dd('ssssss');
+        return view('admin.dashboard');
+    }
+
+}
---
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Auth;
+use Illuminate\Http\Request;
+
+class AdminLoginController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('guest:admin', ['except' => ['logout']]);
+    }
+    public function showLoginForm()
+    {
+        return view('admin.auth.login');
+    }
+    public function login(Request $request)
+    {
+        // dd($request->all());
+        $this->validate($request, [
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)) {
+            return redirect()->intended(route('admin.dashboard'));
+        }
+        return redirect()->back()->withInput($request->only('email', 'remember'));
+    }
+    public function logout()
+    {
+        Auth::guard('admin')->logout();
+        return redirect('admin/login');
+    }
+}
